990ee84bf0c5ed8b49fcda9b8ae0ed6891b2df5f
phamson02 <phamtienson02@gmail.com>
2021-12-25 22:17:29 +0700

    update solution printer

--- src/project9/solver/heuristic_greedy.py
+++ src/project9/solver/heuristic_greedy.py
@@ -1,4 +1,6 @@
 import numpy as np
+import time
+
 
 def heuristic_greedy(data):
     '''
@@ -11,9 +13,11 @@ def heuristic_greedy(data):
     data : Data
         The data to be used in the problem.
     '''
+    start_time = time.time()
+
     # Initialize the solution
-    x = np.array([0 for k in range(data.K)]) # working time of staff k
-    y = {k:[0] for k in range(data.K)} # customers that staff k serves
+    x = np.array([0 for k in range(data.K)])  # working time of staff k
+    y = {k: [0] for k in range(data.K)}  # customers that staff k serves
 
     customers = [n for n in range(1, data.N+1)]
     while len(customers) != 0:
@@ -26,8 +30,8 @@ def heuristic_greedy(data):
         # Find the next customer with the smallest travel time + fix time
         knowledge = []
         for l in customers:
-            knowledge.append((l, data.t[i][l] + data.d[l]))
-        knowledge.sort(key = lambda item: item[1])
+            knowledge.append((l, data.t[i][l] + data.d[l-1]))
+        knowledge.sort(key=lambda item: item[1])
 
         # Assign that customer to the technician and update the solution
         j = knowledge[0][0]
@@ -37,12 +41,21 @@ def heuristic_greedy(data):
         # Remove the customer from the list of customers
         customers.remove(j)
 
-    # for k in range(data.K):
-    #     x[k] += data.t[y[k][-1]][0]
-    #     y[k].append(0)
-
-    print(f'Optimal cost: {np.max(x)}')
     for k in range(data.K):
-        print('Route', k+1,':', end=' ')
-        print(*y[k], sep=' -> ', end=' | ')
-        print(f'cost = {x[k]}')
+        x[k] += data.t[y[k][-1]][0]
+        y[k].append(0)
+
+    print(f'Solving time: {time.time() - start_time}')
+    print(f'Optimal cost: {np.max(x)}')
+
+    for k in range(data.K):
+        journey = f'Route[{k}] = ' + ' -> '.join(str(e) for e in y[k])
+        fix_time = f'fix time = {sum(data.d[e-1] if e != 0 else 0 for e in y[k])}'
+        travel_time = f'travel time = {sum(data.t[i][j] for i, j in zip(y[k], y[k][1:]))}'
+        total_time = f' total time = {x[k]}'
+        print(f'{journey} | {fix_time} | {travel_time} | {total_time}')
+        for i, j in zip(y[k], y[k][1:]):
+            s1 = f'{i} -> {j}'
+            s2 = f'travel time = {data.t[i][j]}'
+            s3 = f'fix time = {data.d[j-1]}' if j != 0 else 'fix time = 0'
+            print(f'{s1} | {s2} | {s3}')
